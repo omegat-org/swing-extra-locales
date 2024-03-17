00894bf8148ee856d8c69a1912a63be3fd707736
Hiroshi Miura <miurahr@noreply.codeberg.org>
2024-03-17 02:38:22 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,150 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    java
+    signing
+    `maven-publish`
+    alias(libs.plugins.spotbugs)
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.git.version)
+    alias(libs.plugins.nexus.publish)
+}
+
+val dotgit = project.file(".git")
+if (dotgit.exists()) {
+    apply(plugin = libs.plugins.git.version.get().pluginId)
+    val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+    val details = versionDetails()
+    val baseVersion = details.lastTag.substring(1)
+    version = when {
+        details.isCleanTag -> baseVersion
+        else -> baseVersion + "-" + details.commitDistance + "-" + details.gitHash + "-SNAPSHOT"
+    }
+} else {
+    val gitArchival = project.file(".git-archival.properties")
+    val props = Properties()
+    props.load(FileInputStream(gitArchival))
+    val versionDescribe = props.getProperty("describe")
+    val regex = "^v\\d+\\.\\d+\\.\\d+$".toRegex()
+    version = when {
+        regex.matches(versionDescribe) -> versionDescribe.substring(1)
+        else -> versionDescribe.substring(1) + "-SNAPSHOT"
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes("Automatic-Module-Name" to "tokyo.northside.texparser")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            groupId = "tokyo.northside"
+            artifactId = "texparser"
+            pom {
+                name.set("example")
+                description.set("Example Library")
+                url.set("https://codeberg.org/miurahr/example")
+                licenses {
+                    license {
+                        name.set("The GNU General Public License, Version 3")
+                        url.set("https://www.gnu.org/licenses/licenses/gpl-3.html")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("miurahr")
+                        name.set("Hiroshi Miura")
+                        email.set("miurahr@linux.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://codeberg.org/miurahr/example.git")
+                    developerConnection.set("scm:git:git://codeberg.org/miurahr/example.git")
+                    url.set("https://codeberg.org/miurahr/example")
+                }
+            }
+        }
+    }
+}
+
+val signKey = listOf("signingKey", "signing.keyId", "signing.gnupg.keyName").find { project.hasProperty(it) }
+tasks.withType<Sign> {
+    onlyIf { signKey != null && !rootProject.version.toString().endsWith("-SNAPSHOT") }
+}
+
+signing {
+    when (signKey) {
+        "signingKey" -> {
+            val signingKey: String? by project
+            val signingPassword: String? by project
+            useInMemoryPgpKeys(signingKey, signingPassword)
+        }
+        "signing.keyId" -> { /* do nothing */
+        }
+        "signing.gnupg.keyName" -> {
+            useGpgCmd()
+        }
+    }
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.withType<Javadoc>() {
+    setFailOnError(false)
+    options {
+        jFlags("-Duser.language=en")
+    }
+}
+
+nexusPublishing.repositories {
+    sonatype()
+}
+
+tasks.withType<Copy> {
+    duplicatesStrategy = DuplicatesStrategy.WARN
+}
+
+tasks.withType<Jar> {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
+
+spotless {
+    format("misc") {
+        target(listOf("*.gradle", ".gitignore"))
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+    java {
+        target(listOf("src/*/java/**/*.java"))
+        palantirJavaFormat()
+        importOrder()
+        removeUnusedImports()
+        formatAnnotations()
+    }
+}
