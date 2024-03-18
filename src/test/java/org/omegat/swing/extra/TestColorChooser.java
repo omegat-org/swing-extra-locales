88f3b95b9fd2d4504f767ee6283a6ba49f605c89
Hiroshi Miura <miurahr@linux.com>
2024-03-18 11:24:00 +0900

    feat: test JColorChooser

---
+++ src/test/java/org/omegat/swing/extra/TestColorChooser.java
@@ -0,0 +1,41 @@
+package org.omegat.swing.extra;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Test;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestColorChooser extends AssertJSwingJUnitTestCase {
+
+    protected FrameFixture window;
+    protected JFrame parent;
+
+    @Override
+    protected void onSetUp() {
+        Locales.initialize(new Locale("ru"));
+        parent = GuiActionRunner.execute(() -> {
+            JFrame frame = new JFrame();
+            frame.setPreferredSize(new Dimension(800, 600));
+            return frame;
+        });
+        window = new FrameFixture(robot(), parent);
+        window.show();
+    }
+
+    @Test
+    public void testColorChooserDialog() {
+        String[] titles = {"Образцы" /* Samples */, "HSV", "HSL", "RGB", "CMYK"};
+        SwingUtilities.invokeLater(() -> JColorChooser.showDialog(parent, "Color", Color.RED));
+        window.dialog().requireVisible();
+        window.dialog().requireModal();
+        assertEquals("Color", window.dialog().target().getTitle());
+        window.dialog().tabbedPane().requireTabTitles(titles);
+    }
+
+}
