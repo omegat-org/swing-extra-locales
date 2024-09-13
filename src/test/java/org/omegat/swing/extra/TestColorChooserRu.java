02007ee05009bc62ce5b1a25c69ba42ab81b9737
Hiroshi Miura <miurahr@linux.com>
2024-09-13 12:20:56 +0900

    feat: update ColorChooser test

--- src/test/java/org/omegat/swing/extra/TestColorChooserRu.java
+++ src/test/java/org/omegat/swing/extra/TestColorChooserRu.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertNotNull;
 import java.awt.*;
 import java.util.Locale;
 import javax.swing.*;
+
+import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
@@ -34,13 +36,45 @@ public class TestColorChooserRu extends AssertJSwingJUnitTestCase {
     @Test
     public void testColorChooserDialog() {
         String[] titles = {"Образцы" /* Samples */, "HSV", "HSL", "RGB", "CMYK"};
-        String rgbText = UIManager.getString("ColorChooser.rgbText");
-        assertEquals("RGB", rgbText);
         SwingUtilities.invokeLater(() -> JColorChooser.showDialog(parent, "Color", Color.RED));
         window.dialog().requireVisible();
         window.dialog().requireModal();
         assertEquals("Color", window.dialog().target().getTitle());
         assertEquals("ru", window.dialog().target().getLocale().getLanguage());
+        // there is a reset button
+        window.dialog().button((new JButtonTextMatcher("Ресет"))).requireEnabled();
+        // there is a cancel button
+        window.dialog().button((new JButtonTextMatcher("Отмена"))).requireEnabled();
+        //
         window.dialog().tabbedPane().requireTabTitles(titles);
+        window.dialog().tabbedPane().selectTab(titles[1]);
+        window.dialog().toggleButton(new JToggleButtonTextMatcher("Сатурация"));
+    }
+
+    private static class JButtonTextMatcher extends GenericTypeMatcher<JButton> {
+        private final String text;
+        public JButtonTextMatcher(String text) {
+            super(JButton.class);
+            this.text = text;
+        }
+
+        @Override
+        protected boolean isMatching(JButton button) {
+            // Check if the button has no name
+            return button.getText().equals(text) && button.isShowing();
+        }
+    }
+
+    private static class JToggleButtonTextMatcher extends GenericTypeMatcher<JToggleButton> {
+        private final String text;
+        public JToggleButtonTextMatcher(String text) {
+            super(JToggleButton.class);
+            this.text = text;
+        }
+
+        @Override
+        protected boolean isMatching(JToggleButton jToggleButton) {
+            return jToggleButton.getText().equals(text) && jToggleButton.isShowing();
+        }
     }
 }
