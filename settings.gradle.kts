103938d05a733ae38890a145da13c7b785efd339
Hiroshi Miura <miurahr@linux.com>
2024-09-14 20:42:42 +0900

    chore: tweak develocity configurations

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,13 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
     id("com.gradle.develocity") version "3.18"
 }
+
+val isCI = !System.getenv("envIsCi").isNullOrEmpty()
+
 develocity {
     buildScan {
-        publishing.onlyIf { "true".equals(System.getProperty("envIsCi")) }
+        uploadInBackground.set(!isCI)
+        publishing.onlyIf { it.buildResult.failures.isNotEmpty() && isCI }
         termsOfUseUrl = "https://gradle.com/terms-of-service"
         termsOfUseAgree = "yes"
     }
