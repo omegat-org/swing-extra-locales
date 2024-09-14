a4ac9330a5df7258c20b9b533ee7aafa8328ec8e
Hiroshi Miura <miurahr@linux.com>
2024-09-14 11:42:44 +0900

    refactor: ExtraLocales class
    - Format codes
    - Add and update javadoc
    - Remove locale field and give it as argument for private methods
    - Add comments
    - Drop suppression for unused warning

--- src/main/java/org/omegat/swing/extra/ExtraLocales.java
+++ src/main/java/org/omegat/swing/extra/ExtraLocales.java
@@ -1,39 +1,72 @@
 package org.omegat.swing.extra;
 
-import org.openide.awt.Mnemonics;
-
-import javax.swing.*;
-import javax.swing.plaf.metal.MetalLookAndFeel;
-import javax.swing.plaf.synth.SynthLookAndFeel;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.synth.SynthLookAndFeel;
+import org.openide.awt.Mnemonics;
 
 /**
  * @author Hiroshi Miura
  */
 public final class ExtraLocales {
-    private static final String EXTRA_BASIC = "org/omegat/swing/extra/basic";
-    // LAF specific
+    // Bundle paths.
+    static final String EXTRA_BASIC = "org/omegat/swing/extra/basic";
+    static final String EXTRA_AQUA = "org/omegat/swing/extra/aqua";
+    static final String EXTRA_GTK = "org/omegat/swing/extra/gtk";
+    static final String EXTRA_WINDOWS = "org/omegat/swing/extra/windows";
+
+    // LaF IDs.
     private static final String AQUA_LAF = "Aqua";
-    private static final String EXTRA_AQUA = "org/omegat/swing/extra/aqua";
     private static final String GTK_LAF = "GTK";
-    private static final String EXTRA_GTK = "org/omegat/swing/extra/gtk";
     private static final String WINDOWS_LAF = "Windows";
-    private static final String EXTRA_WINDOWS = "org/omegat/swing/extra/windows";
     private static final String MOTIF_LAF = "Motif";
+
+    // other bundles.
     private static final String EXTRA_MOTIF = "org/omegat/swing/extra/motif";
     private static final String EXTRA_SYNTH = "org/omegat/swing/extra/synth";
     private static final String EXTRA_METAL = "org/omegat/swing/extra/metal";
 
-    // JRE supports {"zh_CN", "zh_TW", "en", "de", "fr", "it", "es", "pt_BR", "ko", "ja", "sv"};
-    private static Locale locale;
-    private static final String[] supported = {"ar", "ca", "da", "ru", "uk"};
+    // Supported extra languages.
+    // FYI: JRE supports {"zh_CN", "zh_TW", "en", "de", "fr", "it", "es", "pt_BR", "ko", "ja", "sv"};
+    static final String[] supported = {"ar", "ca", "da", "ru", "uk"};
 
-    private static Boolean initialized = false;
-
+    /**
+     * We don't allow instantiates the utility class.
+     */
     private ExtraLocales() {}
 
-    @SuppressWarnings("unused")
+    // Force run initialization only once.
+    private static Boolean initialized = false;
+
+    /**
+     * Initialize localized SWING UI messages.
+     * <p>
+     *     ExtraLocales#initialize loads localized messages
+     *     when the library support the JVM locale.
+     *     It also override messages for LaF specific UI texts
+     *     when the library support it.
+     * </p>
+     * <p>
+     *     The library support the following languages;
+     *     <ul>
+     *         <li>Arabic</li>
+     *         <li>Catalan</li>
+     *         <li>Dansk</li>
+     *         <li>Russian</li>
+     *         <li>Ukrainian</li>
+     *     </ul>
+     *
+     *     It support the following Look-and-Feels;
+     *     <ul>
+     *         <li>Aqua (macOS)</li>
+     *         <li>Gtk (Linux)</li>
+     *         <li>Windows (MS Windows)</li>
+     *     </ul>
+     * </p>
+     */
     public static void initialize() {
         if (initialized) {
             return;
@@ -56,10 +89,12 @@ public final class ExtraLocales {
 
     /**
      * Initializer with locale.
-     * @param newLocale locale for test.
+     * <p>
+     *     It is package-private for test purpose.
+     *
+     * @param locale to load.
      */
-    static void initialize(Locale newLocale) {
-        locale = newLocale;
+    static void initialize(Locale locale) {
         loadLocalizeOverrides(EXTRA_BASIC, locale);
 
         // Laf specific initialization.
@@ -90,7 +125,7 @@ public final class ExtraLocales {
             String val = bundle.getString(key);
             if (!val.isEmpty()) {
                 if (key.endsWith(".textAndMnemonic")) {
-                    processMnemonics(key, val);
+                    processMnemonics(key, val, locale);
                 } else {
                     UIManager.put(key, val);
                 }
@@ -103,7 +138,7 @@ public final class ExtraLocales {
         {"Text", "Mnemonic", "MnemonicIndex"}
     };
 
-    private static void processMnemonics(String key, String val) {
+    private static void processMnemonics(String key, String val, Locale locale) {
         String prefix = key.substring(0, key.length() - ".textAndMnemonic".length());
         int i = prefix.lastIndexOf('.') < 0 ? 0 : 1;
         int n = Mnemonics.findMnemonicAmpersand(val);
@@ -115,7 +150,7 @@ public final class ExtraLocales {
             UIManager.put(prefix + postfixes[i][2], -1);
         } else {
             UIManager.put(prefix + postfixes[i][0], val.substring(0, n) + val.substring(n + 1));
-            int ch = getMnemonicChr(val.charAt(n));
+            int ch = getMnemonicChr(val.charAt(n), locale);
             if (ch > 0) {
                 UIManager.put(prefix + postfixes[i][1], Integer.toString(ch, 10));
                 UIManager.put(prefix + postfixes[i][2], n);
@@ -123,7 +158,7 @@ public final class ExtraLocales {
         }
     }
 
-    private static int getMnemonicChr(char ch) {
+    private static int getMnemonicChr(char ch, Locale locale) {
         if ((ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
             return ch;
         } else if (ch >= 'a' && ch <= 'z') {
